891e8300e18977ee721a0eab2a2c2254c9da5598
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-06-09 16:44:10 +0800

    adder basic video view

---
+++ src/components/Video/VideoPlayer.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+export default class VideoPlayer extends Vue{
+  @Prop() private id!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Video.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import VideoPlayer from "@/components/Video/VideoPlayer.vue";
+
+@Component({
+  components: {
+    VideoPlayer
+  }
+})
+export default class Video extends Vue {}
+</script>
