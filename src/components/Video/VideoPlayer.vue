c7bee50587f98efa0664a9f7d77c0c3dd688408c
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-06-09 18:10:51 +0800

    added in basics for loading video

--- src/components/Video/VideoPlayer.vue
+++ src/components/Video/VideoPlayer.vue
@@ -1,15 +1,48 @@
 <template>
-  
+  <div>
+    <h3>Video with ID: {{ videoId }}</h3>
+    <video ref="mainPlayer" class="video-js"/>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import videojs from 'video.js';
 
-export default class VideoPlayer extends Vue{
-  @Prop() private id!: string;
+@Component
+export default class VideoPlayer extends Vue {
+  @Prop() private videoId!: string;
+  @Prop() private options!: object;
+  player: any;
+
+  data() {
+    return {
+      player: '',
+    };
+  }
+
+  mounted() {
+    this.setupPlayer();
+    console.log(`video id : ${this.videoId}`);
+  }
+
+  onPlayerReady(): any {
+    console.log('Player ready', this);
+  }
+
+  setupPlayer(): any {
+    this.player = videojs(this.$refs.mainPlayer, this.options, () =>
+      this.onPlayerReady(),
+    );
+  }
+
+  private beforeDestory() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,13 +1,18 @@
-<template></template>
+<template>
+  <div class="video-page">
+    <h3>Vue Video</h3>
+    <VideoPlayer :videoId="this.$route.params.id"/>
+  </div>
+</template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import VideoPlayer from "@/components/Video/VideoPlayer.vue";
+import { Component, Vue } from 'vue-property-decorator';
+import VideoPlayer from '@/components/Video/VideoPlayer.vue';
 
 @Component({
   components: {
-    VideoPlayer
-  }
+    VideoPlayer,
+  },
 })
 export default class Video extends Vue {}
 </script>
